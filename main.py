491e140b3bf47d87edd39272c1e1445d1fdb851c
Nikkolas Irwin <irwin.nikk@gmail.com>
2019-04-26 18:25:46 -0700

    updated main.py to use Flask to process incoming JSON data and write it to a word document, added boilerplate code for all word documents used by ARIA

--- main.py
+++ main.py
@@ -1,95 +1,229 @@
-import json
-import student
+from flask import Flask, request
 from docx import Document
 from docx.shared import Inches
 
-student_json_data = '{"1": ["John", "Smith", "16", "A", "B", "C", "Mrs. Wiz", "04/30/2019 at 5:00 PM"]}'
+# Create the Flask app.
+app = Flask(__name__)
 
-student_dict = json.loads(student_json_data)
+# Set the route to 'gen-docs' and only accept POST requests
+@app.route('/gen-docs', methods=['POST'])
+def gen_docs():
+    # Create document variables to hold processing status.
+    # By default they are set to not being requested.
+    # If the document is requested it will be processed successfully or unsuccessfully.
+    # The string will be updated either way.
+    adjudication_form = 'document was not requested.'
+    announcing_sheet = 'document was not requested.'
+    festival_certificates = 'document was not requested.'
+    master_judge_repertoire = 'document was not requested.'
+    master_judge_schedule = 'document was not requested.'
+    results_sheet = 'document was not requested.'
+    room_schedules = 'document was not requested.'
+    session_assignments = 'document was not requested.'
+    session_labels = 'document was not requested.'
+    teacher_master = 'document was not requested.'
 
-def main():
-    participant = student.Student(id=list(student_dict.keys())[0], first_name=student_dict['1'][0],
-                                  last_name=student_dict['1'][1], age=student_dict['1'][2],
-                                  song_1=student_dict['1'][3], song_2=student_dict['1'][4],
-                                  song_3=student_dict['1'][4], teacher_name=student_dict['1'][5],
-                                  performance_time=student_dict['1'][6])
+    # Read the incoming JSON data and store it in 'data'
+    req_data = request.get_json()
 
-    # print('ID:', participant.get_id())
-    # print('First Name:', participant.get_first_name())
-    # print('Last Name:', participant.get_last_name())
-    # print('Age:', participant.get_age())
-    # print('Song 1:', participant.get_song_1())
-    # print('Song 2:', participant.get_song_2())
-    # print('Song 3:', participant.get_song_3())
-    # print('Teacher Name:', participant.get_teacher_name())
-    # print('Performance Time:', participant.get_performance_time())
+    # Determine if the JSON object has data, if it does...process it.
+    try:
+        if (req_data is not None):
+                # -----------------
+                # Adjudication Form
+                # -----------------
+            if (req_data['documents']['2019_upper_festival_adjudication_form'] == True):
+                adjudication_form = create_adjudication_form(req_data)
+                # ----------------
+                # Announcing Sheet
+                # ----------------
+            if (req_data['documents']['2019_upper_festival_announcing_sheet'] == True):
+                announcing_sheet = create_announcing_sheet(req_data)
+                # ---------------------
+                # Festival Certificates
+                # ---------------------
+            if (req_data['documents']['2019_upper_festival_certificates'] == True):
+                festival_certificates = create_festival_certificates(req_data)
+                # -----------------------
+                # Master Judge Repertoire
+                # -----------------------
+            if (req_data['documents']['2019_upper_festival_master_judge_repertoire'] == True):
+                master_judge_repertoire = create_master_judge_repertoire(req_data)
+                # ---------------------
+                # Master Judge Schedule
+                # ---------------------
+            if (req_data['documents']['2019_upper_festival_master_judge_schedule'] == True):
+                master_judge_schedule = create_master_judge_schedule(req_data)
+                # -------------
+                # Results Sheet
+                # -------------
+            if (req_data['documents']['2019_upper_festival_results_sheet'] == True):
+                results_sheet = create_results_sheet(req_data)
+                # --------------
+                # Room Schedules
+                # --------------
+            if (req_data['documents']['2019_upper_festival_room_schedules'] == True):
+                room_schedules = create_room_schedules(req_data)
+                # -------------------
+                # Session Assignments
+                # -------------------
+            if (req_data['documents']['2019_upper_festival_session_assignments'] == True):
+                session_assignments = create_session_assignments(req_data)
+                # --------------
+                # Session Labels
+                # --------------
+            if (req_data['documents']['2019_upper_festival_session_labels'] == True):
+                session_labels = create_session_labels(req_data)
+                # --------------
+                # Teacher Master
+                # --------------
+            if (req_data['documents']['2019_upper_festival_teacher_master'] == True):
+                teacher_master = create_teacher_master(req_data)
+    except Exception as err:
+        return f'Something Went Wrong \n Error Details Below \n, {err}'
+    else:
+        return f'''\n
+                   01. Adjudication Form: {adjudication_form} \n
+                   02. Announcing Sheet: {announcing_sheet} \n
+                   03. Festival Certificates: {festival_certificates} \n
+                   04. Master Judge Repertoire: {master_judge_repertoire} \n
+                   05. Master Judge Schedule: {master_judge_schedule} \n
+                   06. Results Sheet: {results_sheet} \n
+                   07. Room Schedules: {room_schedules} \n
+                   08. Session Assignments: {session_assignments} \n
+                   09. Session Labels: {session_labels} \n
+                   10. Teacher Master: {teacher_master} \n
+        '''
 
-    createSampleDocument(participant)
+def create_adjudication_form(data):
+    try:
+        #
+        #
+        # This document still needs to be built...
+        #
+        #
+        return 'document created successfully.'
+    except Exception as err:
+        return f'document creation failed... \n {err}'
 
-def createSampleDocument(student):
-    document = Document()
 
-    # Upper Festival Title
-    document.add_heading('2019 Upper Festival', level=0)
-    # Festival Subheading
-    document.add_heading('Announcing Sheet', level=1)
-    # Date/Time of Event
-    document.add_heading('Saturday, 9:00 am, 1 (Recital Hall)')
-    # Add Class Type
-    document.add_heading('Master Class')
-    # Add Level
-    document.add_heading('Level 5')
+def create_announcing_sheet(data):
+    try:
+        # Create Document
+        document = Document()
+        # Upper Festival Title
+        document.add_heading(data['event']['event_name'], level=0)
+        # # Festival Subheading
+        # document.add_heading('Announcing Sheet', level=1)
+        # # Day, Time, Session Number, Room
+        # document.add_heading(str(data['event']['sessions']['friday']['day'])), level=1)
+        # # Add Class Type
+        # document.add_heading(str(data['event']['sessions']['friday']['class_type'])), level=1)
+        # # Add Level
+        # document.add_heading(str(data['event']['sessions']['friday']['levels'])), level=1)
+        # Write/Save Document
+        document.save('./documents/announcing_sheet.docx')
 
-    # Add Parapgraph 1 -- Judge
-    p1 = document.add_paragraph()
-    p1.add_run('Judge: \t\t\t\t')
-    p1.add_run('Dr. Stephen Pierce, Univ. of Southern California')
+        return 'document created successfully.'
+    except Exception as err:
+        return f'document creation failed... \n {err}'
 
-    # Add Parapgraph 2 -- Proctor
-    p2 = document.add_paragraph()
-    p2.add_run('Proctor: \t\t\t')
-    p2.add_run('Sonnet Johnson')
 
-    # Add Parapgraph 3 -- Door Monitor
-    p3 = document.add_paragraph()
-    p3.add_run('Door Monitor: \t\t\t')
-    p3.add_run('Catherine Lin')
+def create_festival_certificates(data):
+    try:
+        # !
+        # !
+        # This document still needs to be built...
+        # !
+        # !
+        return 'document created successfully.'
+    except Exception as err:
+        return f'document creation failed... \n {err}'
 
-    # Performance Order
-    document.add_heading('Performance Order', level=1)
-    # Student 1
-    document.add_heading('1. ' + str(student.get_first_name()) + ' ' + str(student.get_last_name()) + '\t\t\t\t\t\t\t Level 5', level=2)
-    p4a = document.add_paragraph()
-    paragraph_format = p4a.paragraph_format
-    paragraph_format.left_indent = Inches(0.5)
-    p4a.add_run('Sea Piece \t\t\t\t\t\t\t MacDowell')
-    p4b = document.add_paragraph()
-    paragraph_format = p4b.paragraph_format
-    paragraph_format.left_indent = Inches(0.5)
-    p4b.add_run('Sonatina for Piano, 1st movement - Bagpipers \t\t Bartok')
 
-    # Student 2
-    document.add_heading('2. ' + str(student.get_first_name()) + ' ' + str(student.get_last_name()) + '\t\t\t\t\t\t\t Level 5', level=2)
-    p5a = document.add_paragraph()
-    paragraph_format = p5a.paragraph_format
-    paragraph_format.left_indent = Inches(0.5)
-    p5a.add_run('Minuet G minor (without Trio) \t\t\t\t Stozel')
-    p5b = document.add_paragraph()
-    paragraph_format = p5b.paragraph_format
-    paragraph_format.left_indent = Inches(0.5)
-    p5b.add_run('Faded Dreams \t\t\t\t\t\t\t Mier')
+def create_master_judge_repertoire(data):
+    try:
+        # !
+        # !
+        # This document still needs to be built...
+        # !
+        # !
+        return 'document created successfully.'
+    except Exception as err:
+        return f'document creation failed... \n {err}'
 
-    # Student 3
-    document.add_heading('3. ' + str(student.get_first_name()) + ' ' + str(student.get_last_name()) + '\t\t\t\t\t\t\t Level 5', level=2)
-    p6a = document.add_paragraph()
-    paragraph_format = p6a.paragraph_format
-    paragraph_format.left_indent = Inches(0.5)
-    p6a.add_run('Four Landlers, G Major \t\t\t\t\t Schubert')
-    p6b = document.add_paragraph()
-    paragraph_format = p6b.paragraph_format
-    paragraph_format.left_indent = Inches(0.5)
-    p6b.add_run('Winter Splendor \t\t\t\t\t\t Mier')
 
-    document.save('sample.docx')
+def create_master_judge_schedule(data):
+    try:
+        # !
+        # !
+        # This document still needs to be built...
+        # !
+        # !
+        return 'document created successfully.'
+    except Exception as err:
+        return f'document creation failed... \n {err}'
 
-main()
+
+def create_results_sheet(data):
+    try:
+        # !
+        # !
+        # This document still needs to be built...
+        # !
+        # !
+        return 'document created successfully.'
+    except Exception as err:
+        return f'document creation failed... \n {err}'
+
+
+def create_room_schedules(data):
+    try:
+        # !
+        # !
+        # This document still needs to be built...
+        # !
+        # !
+        return 'document created successfully.'
+    except Exception as err:
+        return f'document creation failed... \n {err}'
+
+
+def create_session_assignments(data):
+    try:
+        # !
+        # !
+        # This document still needs to be built...
+        # !
+        # !
+        return 'document created successfully.'
+    except Exception as err:
+        return f'document creation failed... \n {err}'
+
+
+def create_session_labels(data):
+    try:
+        # !
+        # !
+        # This document still needs to be built...
+        # !
+        # !
+        return 'document created successfully.'
+    except Exception as err:
+        return f'document creation failed... \n {err}'
+
+
+def create_teacher_master(data):
+    try:
+        # !
+        # !
+        # This document still needs to be built...
+        # !
+        # !
+        return 'document created successfully.'
+    except Exception as err:
+        return f'document creation failed... \n {err}'
+
+
+if __name__ == '__main_':
+    app.run(debug=True, port=4321) #run app in debug mode on port 4321
